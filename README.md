ac2ec9a182db3f00c8ce3d2fc8732e87121f5813
CKrishna Prasad P. Pothuraju <CKrishnaprasad.Pothuraju@techaspect.com>
2016-10-31 12:25:28 +0530

    Working TodoList with Add/Delete Todo items and added roadmap to readme

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Todo-App-Using-VUEJS
-creating a todo list app which uses local storage as datasource.
+
+A Todo List app build using [Vuejs](https://vuejs.org) and styled using [Materializecss](http://materializecss.com)
+
+## Features :
+
+- Add new Todo-Items with Tags.
+- Delete new Todo-Items.
+- Persistent Data with Local Storage.
+
+## Future Plan :
+
+- Reset Inputs after adding New Todo
+- strike Text on checking a list Item.
+- Sort Todo's by read,unread and Tags.
--- app.js
+++ app.js
@@ -1,9 +1,67 @@
-var todoListApp = new Vue({
-   el:"#todoVueApp",
-   created:function() {
-       alert("hello world!");
-   },
-   data:{
-       hw:"Hello Vue!"
-   }
+Vue.component('todo',{
+  template:'#todoCard',
+  props:['todo-Data','index'],
+  methods:{
+    removeTodo:function(){
+        var toRemove = JSON.parse(localStorage.getItem('todoList'));
+        todoUpdated = toRemove.filter(function(val){
+            if(this.todoList.text!=undefined){
+              return val.text!=this.todoList.text
+            }
+        	});
+        localStorage.setItem('todoList',JSON.stringify(todoUpdated));
+        this.$parent.getTodosFromLocalStorage();
+        //this.$parent.resetFields();
+      }
+  }
+});
+
+var todoVM = new Vue({
+  el:'#todoList',
+  created:function(){
+    this.getTodosList();
+  	},
+  methods:{
+    resetFields:function(){
+        this.enteredText='';this.enteredType='';
+    	},
+    getTodosFromLocalStorage:function(){
+      this.todoArray = JSON.parse(localStorage.getItem('todoList'));
+      //this.resetFields();
+    },
+    getTodosList:function(){
+      if (typeof(storage)!== 'undefined') {
+        Materialize.toast('Oops ! there was an error saving your todo. Please try again.', 4000);
+      }else{
+        if (localStorage.getItem('todoList') == undefined || localStorage.getItem('todoList') == null) {
+        Materialize.toast('Nothing to do. Add new Items to work!',4000);
+        }else{
+         this.getTodosFromLocalStorage();
+        }
+      }
+    },
+    addNewTodo:function(){
+      if (typeof(storage)!== 'undefined') {
+        Materialize.toast('Oops ! there was an error saving your todo. Please try again.', 4000);
+      }else{
+        if (localStorage.getItem('todoList') == undefined || localStorage.getItem('todoList') == null) {
+          var targetTodo=[];
+          targetTodo.push({text:this.enteredText,type:this.enteredType});
+          localStorage.setItem('todoList',JSON.stringify(targetTodo));
+          this.getTodosFromLocalStorage();
+        }else{
+          var existingTodoArray = JSON.parse(localStorage.getItem('todoList'));
+          existingTodoArray.unshift({text:this.enteredText,type:this.enteredType});
+          localStorage.setItem('todoList',JSON.stringify(existingTodoArray));
+          this.todoArray = JSON.parse(localStorage.getItem('todoList'));
+          //this.resetFields();
+        }
+      }
+    }
+  },
+  data:{
+    todoArray:[],
+    enteredText:'',
+    enteredType:''
+  }
 });
